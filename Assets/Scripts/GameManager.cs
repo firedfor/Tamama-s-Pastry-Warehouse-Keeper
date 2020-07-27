6190d50079b96cb5dbcc9f758c0bfd2b3be05791
firedfor <rudgnst541@gmail.com>
2020-07-27 20:44:02 +0900

    First Update
    Eat cookies feedback ( Cookies disappear)
    Add screen and cookie shake effect
    Add gameover feedback
    Add animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static ScrollTamama tamama;          // 타마마 객체를 관리하기 위한 변수입니다.
 
-    public static GameManager manager; 
+    public static GameManager manager;              // 게임 매니저 객체를 관리하기 위한 변수입니다.
 
+    public GameObject Tamama;
     public GameObject[,] CookieArray;   // 쿠키들의 관리를 효과적이고 체계적으로 만들어 줄 쿠키 게임 오브젝트들의 배열입니다
     public byte CookieArray_X;                   // 쿠키 게임 오브젝트 배열의 X좌표 입니다
     public byte CookieArray_Y;                   // 쿠키 게임 오브젝트 배열의 Y좌표 입니다
@@ -16,12 +19,14 @@ public class GameManager : MonoBehaviour
     public byte CookiesSpriteArrayNum;      // 쿠키 스프라이트 텍스쳐 배열의 인덱스 요소 개수입니다.
     private SpriteRenderer SpriteRenderer;          // 쿠키 스프라이트 텍스쳐 파일을 게임 오브젝트 내에 입히는 것을 도와주는 변수입니다.
     System.Random r;        // 쿠키 이미지나 음악 등을 랜덤으로 불러올 시 사용하는 변수입니다
+    public Animator Ani;        // 애니메이션 관리 변수입니다.
 
     public bool GameStart = true;             // 게임을 시작하였는지 하지 않았는지 판단하는 변수입니다. 게임 메인 화면에서 마우스 왼쪽 버튼을 클릭해 활성화 할 수 있습니다.
     public bool GameOver = false;                // 게임이 끝났는지 판단하는 변수입니다. 타마마가 과자 밑에 깔릴 시 활성화 됩니다.
 
     void Start()
     {
+        tamama = this.gameObject.GetComponent<ScrollTamama>();
 
         manager = this;
 
@@ -29,42 +34,30 @@ public class GameManager : MonoBehaviour
         CookieArray_Y = 5;
         CookiesSpriteArrayNum = 43;
         r = new System.Random();
+        Ani = GetComponent<Animator>();
 
         CookieArray = new GameObject[CookieArray_Y + 1, CookieArray_X + 1];
         CookiesSpriteArray = new Sprite[CookiesSpriteArrayNum + 1];
 
-        foreach (int y in range(CookieArray_Y))     // 게임 오브젝트 배열 내에 쿠키들의 객체를 담는 과정입니다
-        {
-            foreach (int x in range(CookieArray_X))
-            {
-                CookieArray[y, x] = GameObject.Find("Cookies" + y.ToString() + "-" +  x.ToString());
-            }
-        }
+        Tamama = GameObject.Find("Tamama");
+        AllocateCookies();
 
         CookiesSpriteArray = Resources.LoadAll<Sprite>("Sprite/Cookies");       // 스프라이트 배열 내에 쿠키 이미지들을 담는 과정입니다
 
-
-        foreach (int y in range(CookieArray_Y))     // 게임 오브젝트 스프라이트 내에 쿠키들의 이미지를 담는 과정입니다
-        {
-            foreach (int x in range(CookieArray_X))
-            {
-                SpriteRenderer = CookieArray[y, x].GetComponent<SpriteRenderer>();
-                SpriteRenderer.sprite = CookiesSpriteArray[r.Next(0, 42)];
-            }
-        }
+        ShakeCookie();
         EnableCookies();
     }
 
 
     void Update()
     {
-        
+
     }
 
     public int[] range(int n)  // 파이썬의 range 함수를 모방한 함수입니다. 만약 range(2)를 선언할 시 a[2] = { 1, 2 } 를 반환합니다.
     {
         int[] a = new int[n];
-        for( int i = 1; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
             a[i - 1] = i;
         }
@@ -72,7 +65,17 @@ public class GameManager : MonoBehaviour
         return a;
     }
 
-    public void EnableCookies()
+    public void AllocateCookies(){
+        foreach (int y in range(CookieArray_Y))     // 게임 오브젝트 배열 내에 쿠키들의 객체를 담는 과정입니다
+        {
+            foreach (int x in range(CookieArray_X))
+            {
+                CookieArray[y, x] = GameObject.Find("Cookies" + y.ToString() + "-" +  x.ToString());
+            }
+        }
+    }
+
+    public void EnableCookies()             // 쿠키들을 모두 활성화 시키는 함수입니다.
     {
         foreach (int y in range(5)) 
         {
@@ -81,5 +84,51 @@ public class GameManager : MonoBehaviour
                 CookieArray[y, x].gameObject.SetActive(true);
             }
         }
+    }
+
+    public void ShakeCookie()
+    {
+        foreach (int y in range(CookieArray_Y))     // 게임 오브젝트 스프라이트 내에 쿠키들의 이미지를 담는 과정입니다
+        {
+            foreach (int x in range(CookieArray_X))
+            {
+                SpriteRenderer = CookieArray[y, x].GetComponent<SpriteRenderer>();
+                SpriteRenderer.sprite = CookiesSpriteArray[r.Next(0, 42)];
+            }
+        }
+    }
+
+    public void BeforeBreakCookies()        // 쿠키에 흔들림 효과를 줄 함수입니다
+    {
+
+        for (int y = tamama.YCount + 1; y <= 5; y++)
+        {
+            foreach( int x in range(7))
+            {
+                if(CookieArray[y, x].gameObject.activeSelf == true)
+                {
+                    iTween.ShakePosition(CookieArray[y, x], iTween.Hash("x", 0.5, "y", 0.5, "time", 0.5f));
+                    iTween.ShakePosition(Camera.main.gameObject, iTween.Hash("x", 0.05, "y", 0.05, "time", 0.5f));
+                }
+                
+            }
+        }
+    }
+
+    public void GameOverBreakDown()     // 쿠키를 균형적으로 먹지 않아 무너질 시 발생하는 이벤트 입니다.
+    {
+        GameOver = true;
+        iTween.ShakePosition(Camera.main.gameObject, iTween.Hash("x", 0.5, "y", 0.5 , "time", 0.75f));
+        foreach (int y in range(5))
+        {
+            foreach (int x in range(7))
+            {
+                CookieArray[y, x].gameObject.GetComponent<Rigidbody2D>().simulated = true;
+            }
+        }
+
+        Tamama.gameObject.GetComponent<Animator>().SetBool("GameOver", true);
+        Tamama.gameObject.GetComponent<Rigidbody2D>().simulated = true;
+        
     }
 }
--- Assets/Scripts/ScrollTamama.cs
+++ Assets/Scripts/ScrollTamama.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,16 +14,22 @@ public class ScrollTamama : MonoBehaviour
     public float YTemp;                     // 타마마의 현재 Y 좌표를 임시로 따오는 변수입니다.
     public sbyte XCount;                     // 타마마가 x좌표로 몇 칸 이동했는지 알기 위한 변수입니다.
     public sbyte YCount;                     // 타마마가 y좌표로 몇 칸 이동했는지 알기 위한 변수입니다.
+    public bool[] UnbalancedCookie;                 // 쿠키가 위에 있는데도 불구하고 밑의 쿠키를 먹었는 지 알 수 있는 변수입니다.
+    public bool[] AllOfCookieEaten;                      // 쿠키를 전부 다 먹었는지 알 수 있게 하기 위한 변수입니다.
+    public sbyte[] TempCookieEaten;                    // 쿠키를 전부 다 먹었는지 확인하기 위한 임시 변수입니다.
 
     // Start is called before the first frame update
     void Start()
     {
         XSpeed = 3f;
         YSpeed = 2.5f;
-        XCount = 0;
-        YCount = 0;
+        XCount = 8;
+        YCount = 1;
         TamamaVoiceCount = 0;
         TamamaVoiceCycle = new System.Random();
+        TempCookieEaten = new sbyte[6] { 0, 0, 0, 0, 0, 0 };
+        AllOfCookieEaten = new bool[6] { false, false, false, false, false, false };
+        UnbalancedCookie = new bool[6] { false, false, false, false, false, false };
 
         manager = this.gameObject.AddComponent<GameManager>();
     }
@@ -30,52 +37,121 @@ public class ScrollTamama : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (manager.GameStart == true && Input.GetKeyDown(KeyCode.W) && YCount != 4)
-        {            
+        if (manager.GameStart == true && Input.GetKeyDown(KeyCode.W) && YCount <= 4 && manager.GameOver == false)
+        {
             XTemp = this.gameObject.transform.position.x;                           // 현재 타마마의 x좌표를 temp에 담습니다
             YTemp = this.gameObject.transform.position.y + YSpeed;             // 현재 타마마의 y좌표에 YSpeed를 더해줍니다.
             transform.position = new Vector3(XTemp, YTemp, 0);                  // 구해낸 X, Y 좌표를 타마마의 좌표에 대입합니다
             transform.localScale = new Vector3(0.8f, 0.8f, 1);                          // 이는 Scale_X 좌표에 -1을 곱하면 좌우반전이 되는 것에서 착안하였습니다. 타마마가 오른쪽으로 갈 시 좌우반전 됩니다.
-            YCount++;   
+
+            ++YCount;
+            if (manager.CookieArray[YCount, XCount].gameObject.activeSelf == true && XCount <= 8)
+            {
+                manager.CookieArray[YCount, XCount].gameObject.SetActive(false);
+            }
+
+            BreakDownCheck();
         }
 
-        else if (manager.GameStart == true && Input.GetKeyDown(KeyCode.A) && XCount != 7)
+        else if (manager.GameStart == true && Input.GetKeyDown(KeyCode.A) && XCount >= 2 && manager.GameOver == false)
         {
             XTemp = this.gameObject.transform.position.x - XSpeed;
             YTemp = this.gameObject.transform.position.y;
             transform.position = new Vector3(XTemp, YTemp, 0);
             transform.localScale = new Vector3(0.8f, 0.8f, 1);
-            XCount++;
+
+            XCount--;
+            if (XCount != 8 && manager.CookieArray[YCount, XCount].gameObject.activeSelf == true)
+            {
+                manager.CookieArray[YCount, XCount].gameObject.SetActive(false);
+            }
+
+            BreakDownCheck();
         }
 
-        else if (manager.GameStart == true && Input.GetKeyDown(KeyCode.S) && YCount != 0)
+        else if (manager.GameStart == true && Input.GetKeyDown(KeyCode.S) && YCount >= 2 && manager.GameOver == false)
         {
             XTemp = this.gameObject.transform.position.x;
             YTemp = this.gameObject.transform.position.y - YSpeed;
             transform.position = new Vector3(XTemp, YTemp, 0);
             transform.localScale = new Vector3(0.8f, 0.8f, 1);
-            YCount--;
+
+            --YCount;
+            if (manager.CookieArray[YCount, XCount].gameObject.activeSelf == true && XCount <= 7)
+            {
+                manager.CookieArray[YCount, XCount].gameObject.SetActive(false);
+            }
+
+            BreakDownCheck();
         }
 
-        else if (manager.GameStart == true && Input.GetKeyDown(KeyCode.D) && XCount != 0)
+        else if (manager.GameStart == true && Input.GetKeyDown(KeyCode.D) && XCount <= 7 && manager.GameOver == false)
         {
             XTemp = this.gameObject.transform.position.x + XSpeed;
             YTemp = this.gameObject.transform.position.y;
             transform.position = new Vector3(XTemp, YTemp, 0);
             transform.localScale = new Vector3(-0.8f, 0.8f, 1);
-            XCount--;
+
+            XCount++;
+            if (XCount != 8 && manager.CookieArray[YCount, XCount].gameObject.activeSelf == true)
+            {
+                manager.CookieArray[YCount, XCount].gameObject.SetActive(false);
+            }
+
+            BreakDownCheck();
         }
 
 
-        
+
     }
 
-    void trigger (Collider target)
+    //void OnTriggerEnter2D(Collider2D target)
+    //{
+    //    if (target.gameObject.tag == "Cookie" && target.gameObject.activeSelf == true)
+    //    {
+    //        Debug.Log("Collider");
+    //        target.gameObject.SetActive(false);
+    //    }
+
+    //}
+
+    void BreakDownCheck()       // 과자가 불균형하여 무너지는지 판단하여주는 함수입니다.
     {
-        if (target.tag == "Cookie" && target.gameObject.activeSelf == true)
+        if( manager.CookieArray[YCount, XCount].gameObject.activeSelf == false)
         {
-            target.gameObject.SetActive(false); 
-        }
+            foreach (int y in manager.range(manager.CookieArray_Y))
+                    {
+                        foreach (int x in manager.range(manager.CookieArray_X))
+                        {
+                            if (manager.CookieArray[y, x].gameObject.activeSelf == false)
+                            {
+                                TempCookieEaten[y]++;
+                            }
+                        }
+            }
+
+                    for (int x = 1; x <= 5; x++)
+                    {
+                        if(TempCookieEaten[x] == 7)
+                        {
+                            AllOfCookieEaten[x] = true;
+                        }
+                    }
+
+                    if(YCount != 5)
+                    {
+                        if (AllOfCookieEaten[YCount + 1] == false && AllOfCookieEaten[YCount] == true)
+                        {
+                            manager.GameOverBreakDown();
+                        }
+                        else if(AllOfCookieEaten[YCount + 1] == false && TempCookieEaten[YCount] > 0)
+                        {
+                            manager.BeforeBreakCookies();
+                        }
+                    }
         
+                    TempCookieEaten = new sbyte [6] { 0, 0, 0, 0, 0, 0 };
+        }
     }
+
 }
